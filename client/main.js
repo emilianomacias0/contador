597cb30862e67f6875bd55c0746c4ea7e754a44b
emiliano macias <emilianomacias0@gmail.com>
2016-07-17 18:44:27 -0500

    Se agrego control de cuentas de usuario

--- client/main.js
+++ client/main.js
@@ -1,13 +1,20 @@
 Template.formulario.events({
 	'submit form':function(evt){
 		evt.preventDefault();
-
+		var usuario = Meteor.userId();
 		var ubicacion = evt.target.ubicacion.value;
 		var alias = evt.target.alias.value;
 		var tipo = evt.target.tipo.value;
 		var procentajeComision = parseInt(evt.target.comision.value);
 		var denominacion = parseInt(evt.target.denominacion.value);
-		Maquinas.insert({tipo:tipo,alias:alias,ubicacion:ubicacion,comision:procentajeComision,denominacion:denominacion,creadoEl:new Date()});
+		Maquinas.insert({
+			tipo:tipo,alias:alias,
+			ubicacion:ubicacion,
+			comision:procentajeComision,
+			denominacion:denominacion,
+			creadoPor:usuario,
+			creadoEl:new Date()
+		});
 		evt.target.ubicacion.value = '';
 		evt.target.alias.value='';
 		evt.target.tipo.value='';
@@ -21,7 +28,7 @@ Template.formulario.events({
 
 Template.menu.helpers({
 	maquinas:function(){
-		return Maquinas.find();
+		return Maquinas.find({});
 	}
 });
 
@@ -41,6 +48,42 @@ Template.datosMaquina.events({
 	Session.set('denominacion',denominacion);
 	var salida = evt.target.value;
 	Session.set('salida',salida);
+},
+'submit form':function(evt){
+	evt.preventDefault();
+	var fecha = new Date(evt.target.fecha.value);
+	var conEnt = parseInt(evt.target.conEntrada.value);
+	var conSal = parseInt(evt.target.conSalida.value);
+	var totalRec = accounting.unformat(evt.target.total_rec.value);
+	var totalCom = accounting.unformat(evt.target.total_com.value);
+	var total = accounting.unformat(evt.target.total.value);
+	var id = this._id;
+	if(Session.get('total') != 0){
+	if(confirm('Los Datos son Correctos?')){
+			Contadores.insert({
+		idMaquina:id,
+		fecha:fecha,
+		contadores_entrada:conEnt,
+		contadores_salida:conSal,
+		total_recaudado:totalRec,
+		total_comision:totalCom,
+		total:total,
+		creadoEl:new Date()
+	});
+			Session.set('comision',0);
+			Session.set('denominacion',0);
+			Session.set('salida',0);
+			Session.set('entrada',0);
+			Session.set('total_recaudado',0);
+			Session.set('total',0);
+			evt.target.fecha.value = '';
+			evt.target.conSalida.value = '';
+			evt.target.conEntrada.value ='';
+
+		}
+	}else{
+		alert("Debes llenar todos los campos");
+	}
 }
 });
 
--- collections/collection.js
+++ collections/collection.js
@@ -1,2 +1,20 @@
 Contadores = new Mongo.Collection('contadores');
+
 Maquinas = new Mongo.Collection('maquinas');
+if (Meteor.isServer) {
+	Meteor.publish('LaMaquinas',function(){
+	var usuario = this.userId;
+	return Maquinas.find({creadoPor:usuario});
+});
+Meteor.publish('LosContadores',function(){
+	return Contadores.find();
+});
+
+}
+
+
+if (Meteor.isClient) {
+	Meteor.subscribe('LaMaquinas');
+	Meteor.subscribe('LosContadores');
+}
+
